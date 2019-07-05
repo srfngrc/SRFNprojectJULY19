2c5435aeb422008422f881ebca9ddb32bf971646
sg <srfngrc@outlook.com>
2019-07-05 17:45:18 -0400

    1st upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRFNprojectJULY19v1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start of the TAX program!!!");
+            Console.WriteLine("----------------------------------------------------------------------");
+            Console.ReadLine();
+        }
+    }
+}
